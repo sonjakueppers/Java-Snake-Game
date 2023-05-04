49d84d5ac878f6a361850bc2efa9b69cf1351d38
couldbecoral <125914545+couldbcoral@users.noreply.github.com>
2023-05-03 20:26:49 -0400

    fast snake

--- src/com/zetcode/Board.java
+++ src/com/zetcode/Board.java
@@ -17,12 +17,12 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 1000;
-    private final int B_HEIGHT = 100;
+    private final int B_WIDTH = 500;
+    private final int B_HEIGHT = 500;
     private final int DOT_SIZE = 10;
     private final int ALL_DOTS = 900;
-    private final int RAND_POS = 4;
-    private final int DELAY = 140;
+    private final int RAND_POS = 50;
+    private final int DELAY = 30;
 
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
